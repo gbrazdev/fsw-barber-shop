d57d9030a6441c3cedc6c27f5fbde6efdc542410
Gabriel <gabrielbrazdev@gmail.com>
2024-08-13 16:46:48 -0300

    #fix: route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,9 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
